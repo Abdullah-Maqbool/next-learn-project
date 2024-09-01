6c44272c5e5c81e1d046de38965584db9ac3123e
Abdullah Maqbool <abdullah.maqbool.ahmad@gmail.com>
2024-09-01 16:56:42 +0500

    Prompt Card User Image fix

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -22,7 +22,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div className="w-10 flex-auto flex justify-start items-center gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
             alt="user_image"
